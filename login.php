7855422b365328cb6afb7adb79afbe016f375b5b
yazan-abuhammad <yazanjamalabuhammad@gmail.com>
2023-08-08 01:55:08 +0300

    ok

---
+++ login.php
@@ -0,0 +1,42 @@
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="login.php" method="post">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">username</label>
+            <input type="username" class="form-control" name="username" id="exampleInputusername" aria-describedby="usernameHelp">
+            <div id="usernameHelp" class="form-text">We'll never share your username with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" id="exampleInputPassword1">
+        </div>
+
+        <button type="submit" class="btn btn-primary" value="Submit" name="Submit">Submit </button>
+    </form>
+
+    <?php
+    if (isset($_POST['Submit'])); {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+    }
+    $query = "INSERT INTO users (username,password)";
+    $query .= "VALUES ('$username','$password')";
+    mysqli_query($connection, $query);
+
+    ?>
+
+</body>
+
+</html>
